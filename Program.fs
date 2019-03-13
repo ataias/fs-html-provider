97fa2966b954557f21a0f7064e7a4945be25170c
Ataias Pereira Reis <ataiasreis@gmail.com>
2019-03-13 06:50:48 -0300

    Add Suave

--- Program.fs
+++ Program.fs
@@ -1,8 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
+// Code here was written following book Expert F# 4.0
 
 open System
 open FSharp.Data
-
+open Suave
+open Suave.Http
+open Suave.Web
 
 type Species = HtmlProvider<"https://en.wikipedia.org/wiki/The_world's_100_most_threatened_species">
 
@@ -13,9 +16,17 @@ let printTupleArray t_array =
 [<EntryPoint>]
 let main argv =
     let species = [ for x in Species.GetSample().Tables.``Species list``.Rows -> x.Type, x.``Common name`` ]
-    let processed =
+    let speciesSorted =
       species
         |> List.countBy fst
         |> List.sortByDescending snd
-    printTupleArray processed
+    printTupleArray speciesSorted
+    let html =
+      String.concat "\n" [
+        yield "<html><body><ul>"
+        for (category, count) in speciesSorted do
+          yield sprintf "<li>Category <b>%s</b>: <b>%d</b></li>" category count
+        yield "</ul></body></html>"
+      ]
+    startWebServer defaultConfig (Successful.OK html)
     0 // return an integer exit code
